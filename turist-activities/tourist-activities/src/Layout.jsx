4da50d8364e117ff0fd8e25498183ccd7b6c93fe
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-03-12 15:10:45 +0100

    css del header (burger)

--- turist-activities/tourist-activities/src/Layout.jsx
+++ turist-activities/tourist-activities/src/Layout.jsx
@@ -2,6 +2,8 @@ import {Header} from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import {Outlet} from 'react-router'
 import '@/css/index.css'
+import '@/css/header.css'
+import '@/css/footer.css'
 
 function Layout() {
 
--- turist-activities/tourist-activities/src/components/Header.jsx
+++ turist-activities/tourist-activities/src/components/Header.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react'
 import { NavLink } from "react-router";
 
 export const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
     return (
         <header className="Header">
             <h2 className="Header-title">Header</h2>
-            <NavLink className="Header-link" to={"/secciones"}>Secciones</NavLink>
+            <div className="Hamburger" onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+            <nav className={`Nav-menu ${isOpen ? 'Open' : ''}`}>
+                <NavLink className="Nav-link" to={"/"}>Seccion 1</NavLink>
+                <NavLink className="Nav-link" to={"/"}>sección 2</NavLink>
+                <NavLink className="Nav-link" to={"/"}>Seccion 3</NavLink>
+            </nav>
+
 
         </header>);
 }
