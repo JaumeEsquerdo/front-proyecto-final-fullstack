136bc3aa1acfa391402fc5abbcba7096e8c52dfb
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-04-01 20:30:08 +0200

    css welcomepage

--- turist-activities/tourist-activities/src/pages/WelcomePage.jsx
+++ turist-activities/tourist-activities/src/pages/WelcomePage.jsx
@@ -1,30 +1,36 @@
 import { useEffect } from "react";
 import { useNavigate, Link } from "react-router";
+import '@/css/pages/welcomePage.css'
 
 
 const Welcome = () => {
 
     const navigate = useNavigate() //hook para navegar entre rutas y decir los movimientos
 
-   /**  useEffect(()=>{
-        // para verificar si el usuario ya ha visitado el WelcomePage y pasar directamente al '/home'
-        if(localStorage.getItem('hasVisitedWelcomePage')){
-            navigate('/home')
-        } else{
-            // en caso de ser la primera vez, marco que se ha visitado ya (el true no tiene efecto de comparación pero lo dejo para verlo mas claro)
-            localStorage.setItem('hasVisitedWelcomePage', 'true')
-        }
-    },[navigate])
-    */
+    /**  useEffect(()=>{
+         // para verificar si el usuario ya ha visitado el WelcomePage y pasar directamente al '/home'
+         if(localStorage.getItem('hasVisitedWelcomePage')){
+             navigate('/home')
+         } else{
+             // en caso de ser la primera vez, marco que se ha visitado ya (el true no tiene efecto de comparación pero lo dejo para verlo mas claro)
+             localStorage.setItem('hasVisitedWelcomePage', 'true')
+         }
+     },[navigate])
+     */
 
     return (
         <>
-        <div>
-            <h1>Turistea Villajoyosa</h1>
-            <h2>Planifica tu estancia para aprovechar al máximo de este pequeño paraíso</h2>
-            
-        <Link to={'/home'}>Entra</Link>
-        </div>
+            <section className="WelcomePage">
+                <p className="foto">AQUI VA UNA IMG</p>
+
+                <div className="WelcomePage-content">
+                    <h1 className="WelcomePage-title">Turistea Villajoyosa</h1>
+                    <h2 className="WelcomePage-subtitle">Planifica tu estancia para aprovechar al máximo de este pequeño paraíso</h2>
+
+                    <Link className="WelcomePage-link" to={'/home'}>Entra</Link>
+                </div>
+
+            </section>
         </>
     );
 }
