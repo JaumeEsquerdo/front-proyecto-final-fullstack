a187929462b219a08903dc4f4251ba0f43a4928c
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-04-06 02:58:54 +0200

    intento de motion

--- turist-activities/tourist-activities/src/components/Footer.jsx
+++ turist-activities/tourist-activities/src/components/Footer.jsx
@@ -1,15 +1,53 @@
-import { NavLink } from "react-router";
+import { NavLink } from "react-router-dom";
+import { motion } from 'framer-motion'
+// cuando un NavLink esta activo `isActive` se genera automáticamente 
+
+const navItems = [
+    { name: 'Home', path: '/home' },
+    { name: 'Calendar', path: '/secciones' }
+]
 
 export const Footer = () => {
+
     return (
         <footer className="Footer">
             <nav className="Footer-nav">
                 <ul className="Footer-ul">
-                <NavLink className="Footer-link" to={"/secciones"}>Secciones</NavLink>
-                <NavLink className="Footer-link" to={"/secciones"}>Secciones</NavLink>
+
+                    {navItems.map((item) => {
+
+                        return (
+
+                            <li key={item.path} className="Footer-li">
+                                <NavLink
+                                    className={({ isActive }) => `Footer-link ${isActive ? "active" : ""}`}
+                                    to={item.path}
+
+
+                                >
+                                    
+                                    <p className="Footer-p">{item.name}</p>
+                                    {({ isActive }) => isActive && (
+                                        <motion.div
+                                            layoutId="footer-indicador"
+                                            className="Footer-motions"
+                                            transition={{ type: "spring", stiffness: 500, damping: 30 }}
+                                        >
+                                            <div className="Footer-motion"></div>
+                                        </motion.div>
+                                    )}
+                                    
+                                </NavLink>
+
+
+                            </li>
+                        )
+                    })}
 
                 </ul>
             </nav>
             <div className="Footer-div">+</div>
         </footer>);
 }
+
+
