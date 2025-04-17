03992e7e09bef8dbe4d1dae8738334efdbe19c26
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-04-17 16:59:05 +0200

    update css profile

--- front-tourist-activities/src/pages/Profile.jsx
+++ front-tourist-activities/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
     const navigate = useNavigate();
-    
+
     const handleBack = () => {
         navigate(-1); // volver a la pagina anterior (o pagina de home o calendar)
     }
@@ -11,33 +11,31 @@ const Profile = () => {
     return (
         <>
             <div className="Profile">
-                <header className='Profile-header'>
-                <button className="Nav-link" onClick={handleBack}>
-                <img className='Nav-perfil' src="/img/icono-perfil.svg" alt="Perfil" />
+                <img className='Profile-bg' src="/img/fondo-escena.png" alt="img de fondo" />
+                <button className="Profile-nav" onClick={handleBack}>
+                    <img className='Profile-link' src="/img/flecha-atras.svg" alt="Perfil" />
                 </button>
-                </header>
                 <div className="Profile-card">
                     <h2 className="Profile-title">Tu Perfil</h2>
-                    img?
 
                     <form className="Profile-form">
 
-                        <div className="Profile-field">
-                            <label className="Profile-label">Nombre
-                                <input type="text" className="Profile-input" />
-                            </label>
-                        </div>
 
-                        <div className="Profile-field">
-                            <label className="Profile-label">Email
-                                <input type="email" className="Profile-input" />
-                            </label>
-                        </div>
-                        <div className="Profile-field">
-                            <label className="Profile-label">Contraseña
-                                <input type="password" className="Profile-input" />
-                            </label>
-                        </div>
+                        <label className="Profile-label">Nombre
+                            <input type="text" className="Profile-input" />
+                        </label>
+
+
+
+                        <label className="Profile-label">Email
+                            <input type="email" className="Profile-input" />
+                        </label>
+
+
+                        <label className="Profile-label">Contraseña
+                            <input type="password" className="Profile-input" />
+                        </label>
+
                         <button className="Profile-button" type="submit">Guardar cambios</button>
                     </form>
 
