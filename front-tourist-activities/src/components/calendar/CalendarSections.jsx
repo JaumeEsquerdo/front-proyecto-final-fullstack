da3ea5fe18c8a3ed891fd1f52ee7989aa76618f6
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-04-20 01:29:27 +0200

    update css calendar btn ocultar acts

--- front-tourist-activities/src/components/calendar/CalendarSections.jsx
+++ front-tourist-activities/src/components/calendar/CalendarSections.jsx
@@ -111,7 +111,7 @@ export const ActivityPanel = ({ setShowExtraHours,showExtraHours,selectedDay, vi
         <>
             <div className='Actividades'>
                 <h3 className="Actividades-h3">Actividades para {selectedDay.toLocaleDateString('es-Es', dateOptions)}</h3>
-                <button onClick={() => setShowExtraHours(!showExtraHours)}>
+                <button className="Actividades-btn" onClick={() => setShowExtraHours(!showExtraHours)}>
                     {showExtraHours? 'Ocultar horas extra': 'Mostrar dos las horas'}
                 </button>
                 {visibleHours.map((hour, i) => (
