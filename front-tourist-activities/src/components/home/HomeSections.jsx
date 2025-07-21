6bdbd365b7945737727581955d1091fda0bc8dd3
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-07-21 15:44:19 +0200

    update css de pack detalles abierto

--- front-tourist-activities/src/components/home/HomeSections.jsx
+++ front-tourist-activities/src/components/home/HomeSections.jsx
@@ -18,8 +18,8 @@ export const PackCard = ({ i, pack, handleAbrirPack }) => {
 export const PackDetails = ({ listaDePacks, packAbierto, actividades, setPackAbierto, handleAgregarACalendario }) => {
     return (
         <div className="Pack-overlay" onClick={() => setPackAbierto(null)}>
-            <span className="Pack-cerrar">X</span>
             <div className={`Pack-detalles ${listaDePacks[packAbierto].color}`} onClick={(e) => e.stopPropagation()}>
+                <span className="Pack-cerrar" onClick={() => setPackAbierto(null)}>X</span>
                 <div className="Pack-header">
                     <img className="Pack-icono" src={`/img/pack-recom/${listaDePacks[packAbierto].tipo}.svg`} alt="Icono pack" />
                     <h3 className="Pack-h3">{listaDePacks[packAbierto].nombre}</h3>
