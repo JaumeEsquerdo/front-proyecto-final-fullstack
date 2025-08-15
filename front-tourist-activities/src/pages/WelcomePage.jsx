d4bb150b6ab6cd32ba58ba73431e2600daef172f
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-08-15 11:43:10 +0200

    cambio estructura welcome page

--- front-tourist-activities/src/pages/WelcomePage.jsx
+++ front-tourist-activities/src/pages/WelcomePage.jsx
@@ -13,27 +13,26 @@ const Welcome = () => {
     return (
         <>
 
-            <section className="WelcomePage">
-                <div className="WelcomePage-div">
-                    <div className="WelcomePage-content">
-                        {/* <div className="WelcomePage-lines">
+            <main className="WelcomePage">
+
+                <header className="WelcomePage-header">
+                    {/* <div className="WelcomePage-lines">
                             <span className="WelcomePage-line"></span>
                         </div> */}
-                        
-                        <h1 className="WelcomePage-title">Turistea Villajoyosa</h1>
-                        <div className="WelcomePage-description">
 
-                            <h2 className="WelcomePage-subtitle">Descubre los planes más típicos de 'la Vila' y organízate a tu gusto.
-                            </h2>
-                            <h2 className="WelcomePage-subtitle">  Guarda las actividades que te recomendamos en el calendario, añade las tuyas propias y, sobre todo, vive la experiencia a tu manera.
-                            </h2>
-                        </div>
-                        <Link className="WelcomePage-link" to={'/home'}>Empieza a planificar</Link>
+                    <h1 className="WelcomePage-title">Turistea Villajoyosa</h1>
+                    <div className="WelcomePage-description">
+
+                        <h2 className="WelcomePage-subtitle">Descubre los planes más típicos de 'la Vila' y organízate a tu gusto.
+                        </h2>
+                        <h2 className="WelcomePage-subtitle">  Guarda las actividades que te recomendamos en el calendario, añade las tuyas propias y, sobre todo, vive la experiencia a tu manera.
+                        </h2>
                     </div>
-
-                </div>
-
-            </section>
+                    <Link className="WelcomePage-link" to={'/home'}>Empieza a planificar</Link>
+                </header>
+                
+                <img src="/img/img/vila-foto.jpg" alt="Imagen de la playa de Villajoyosa" />
+            </main>
         </>
     );
 }
