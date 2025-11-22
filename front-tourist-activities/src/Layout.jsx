baf57a6f5792d0ce98beb3196f4fad6c49997002
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-11-22 18:50:45 +0100

    fadeIn en login para evitar petardeo

--- front-tourist-activities/src/Layout.jsx
+++ front-tourist-activities/src/Layout.jsx
@@ -4,14 +4,10 @@ import { Outlet } from "react-router";
 import "@/css/index.css";
 import "@/css/components/header.css";
 import "@/css/components/footer.css";
-// import { useLocation } from "react-router";
 
 import { ActivityProvider } from "./context/ActivityContext";
 
 function Layout() {
-  // const location = useLocation();
-  // const locationIsCalendario = location.pathname.includes("calendario");
-
   return (
     <>
       <ActivityProvider>
--- front-tourist-activities/src/pages/ErrorPage.jsx
+++ front-tourist-activities/src/pages/ErrorPage.jsx
@@ -1,20 +1,17 @@
 import { Link } from "react-router-dom";
 
-
 const ErrorPage = () => {
-    return (
-        <div className="WelcomePage">
-            <img className="WelcomePage-fondo" src="/img/fondo-escena.png" alt="Fondo escena" />
-
-            <div className="Error">
-                <h1>Error 404!!</h1>
-                <h2>Te has equivocado de página...</h2>
-                <Link className="Error-link" to={'/home'}>Prueba de nuevo!</Link>
-            </div>
-
-
-        </div>
-    );
-}
+  return (
+    <div className="WelcomePage">
+      <div className="Error">
+        <h1>Error 404!!</h1>
+        <h2>Te has equivocado de página...</h2>
+        <Link className="Error-link" to={"/home"}>
+          Prueba de nuevo!
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default ErrorPage;
